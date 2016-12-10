5156ca7d380208d2378f2f47c0791b9bab536606
Lammert Bies <github@linocomm.net>
2016-12-10 15:09:46 +0100

    Moved redirect_to_https_port to own file

---
+++ src/httplib_redirect_to_https_port.c
@@ -0,0 +1,76 @@
+/* 
+ * Copyright (C) 2016 Lammert Bies
+ * Copyright (c) 2013-2016 the Civetweb developers
+ * Copyright (c) 2004-2013 Sergey Lyubka
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+
+
+#include "libhttp-private.h"
+
+
+
+/*
+ * void XX_httplib_redirect_to_https_port( struct mg_connection *conn, int ssl_index );
+ *
+ * The function XX_httplib_redirect_to_https_port() redirects a request to an
+ * encrypted connection over HTTPS.
+ */
+
+void XX_httplib_redirect_to_https_port( struct mg_connection *conn, int ssl_index ) {
+
+	char host[1025];
+	const char *host_header;
+	size_t hostlen;
+
+	host_header = mg_get_header(conn, "Host");
+	hostlen = sizeof(host);
+	if (host_header != NULL) {
+		char *pos;
+
+		XX_httplib_strlcpy(host, host_header, hostlen);
+		host[hostlen - 1] = '\0';
+		pos = strchr(host, ':');
+		if (pos != NULL) *pos = '\0';
+	} else {
+		/* Cannot get host from the Host: header.
+		 * Fallback to our IP address. */
+		if (conn) XX_httplib_sockaddr_to_string(host, hostlen, &conn->client.lsa);
+	}
+
+	/* Send host, port, uri and (if it exists) ?query_string */
+	if (conn) {
+		mg_printf(conn,
+		          "HTTP/1.1 302 Found\r\nLocation: https://%s:%d%s%s%s\r\n\r\n",
+		          host,
+#if defined(USE_IPV6)
+		          (conn->ctx->listening_sockets[ssl_index].lsa.sa.sa_family
+		           == AF_INET6)
+		              ? (int)ntohs(conn->ctx->listening_sockets[ssl_index].lsa.sin6.sin6_port)
+		              :
+#endif
+		              (int)ntohs(conn->ctx->listening_sockets[ssl_index].lsa.sin.sin_port),
+		          conn->request_info.local_uri,
+		          (conn->request_info.query_string == NULL) ? "" : "?",
+		          (conn->request_info.query_string == NULL) ? "" : conn->request_info.query_string);
+	}
+
+}  /* XX_httplib_redirect_to_https_port */
