db1e518bb7dd04c5874cccb817a25552750b9d74
Lammert Bies <github@linocomm.net>
2016-12-13 02:30:21 +0100

    No code from others here anymore

--- src/httplib_get_builtin_mime_type.c
+++ src/httplib_get_builtin_mime_type.c
@@ -1,7 +1,5 @@
 /* 
  * Copyright (c) 2016 Lammert Bies
- * Copyright (c) 2013-2016 the Civetweb developers
- * Copyright (c) 2004-2013 Sergey Lyubka
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
