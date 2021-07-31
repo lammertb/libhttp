3e863424c13775df78e2aefed4b396f9c75b3221
Lammert Bies <lammert@freebsd1364.heaven.linocomm.net>
2021-07-31 12:14:33 +0200

    FreeBSD also uses the XSI version of strerror_r

--- src/httplib_error_string.c
+++ src/httplib_error_string.c
@@ -167,7 +167,7 @@ LIBHTTP_API char *httplib_error_string( int error_code, char *buf, size_t buf_le
 
 #else  /* not _WIN32 */
 
-#if ((_POSIX_C_SOURCE >= 200112L || _XOPEN_SOURCE >= 600) && ! defined(_GNU_SOURCE))  /* XSI version of strerror_r */
+#if defined(__FreeBSD__)  ||  ((_POSIX_C_SOURCE >= 200112L || _XOPEN_SOURCE >= 600) && ! defined(_GNU_SOURCE))  /* XSI version of strerror_r */
 
 	int return_val;
 
